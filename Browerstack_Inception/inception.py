856ba016dc61051d63259b9346d4696214a8fc0a
Princeton <princeton99@Princetons-MacBook-Pro.local>
2021-07-09 12:14:35 +0530

    Working Percy and Inception

--- Browerstack_Inception/inception.py
+++ Browerstack_Inception/inception.py
@@ -1,3 +1,4 @@
+from threading import Thread
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
@@ -7,93 +8,155 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv() 
+load_dotenv() 
 
-# ACCESS_KEY = os.getenv('ACCESS_KEY')
+ACCESS_KEY = os.getenv('ACCESS_KEY')
+PASS = os.getenv('USER_PASS')
 
-driver =  webdriver.Chrome()
-driver.maximize_window()
-action = ActionChains(driver)
+caps = [{
+"os" : "OS X",
+"os_version" : "Catalina",
+"browser" : "Edge",
+"browser_version" : "latest",
+'name': 'Inception-Parallel-Run-1', # test name
+ 'build': 'Inception Build ',
+ 'browserstack.use_w3c' : 'true'
+},
+{
+"os" : "OS X",
+"os_version" : "Big Sur",
+"browser" : "Chrome",
+"browser_version" : "latest",
+'name': 'Inception-Parallel-Run-2', # test name
+ 'build': 'Inception Build ',
+ 'browserstack.use_w3c' : 'true'
+},
+{
+"os" : "OS X",
+"os_version" : "Big Sur",
+"browser" : "Edge",
+"browser_version" : "latest",
+'name': 'Inception-Parallel-Run-3', # test name
+ 'build': 'Inception Build ',
+ 'browserstack.use_w3c' : 'true'
+}]
 
-driver.get('https://www.browserstack.com/')
+single_caps = {
+"os" : "OS X",
+"os_version" : "Big Sur",
+"browser" : "Edge",
+"browser_version" : "latest",
+'name': 'Inception-Parallel-Run-3', # test name
+ 'build': 'Inception Build ',
+ 'browserstack.use_w3c' : 'true'
+}
 
-sign_in_btn = '//*[@id="primary-menu"]/li[5]/a'
+def run_session(desired_cap):
+    # driver =  webdriver.Chrome()
+    driver = webdriver.Remote(
+        command_executor='https://princetonbaretto_7D2Tbt:'+ACCESS_KEY+'@hub-cloud.browserstack.com/wd/hub',
+        desired_capabilities=desired_cap)
 
-driver.find_element_by_xpath(sign_in_btn).click()
+    driver.maximize_window()
+    action = ActionChains(driver)
+    try:
+        driver.get('https://www.browserstack.com/')
 
-email_box = 'user_email_login'
-driver.find_element_by_id(email_box).send_keys("princeton.b+demo@browserstack.com")
+        sign_in_btn = '//*[@id="primary-menu"]/li[5]/a'
 
-action.send_keys(Keys.TAB).send_keys('Princeton@99').perform()
+        driver.find_element_by_xpath(sign_in_btn).click()
 
-submit_btn = 'user_submit'
-driver.find_element_by_id(submit_btn).click()
+        email_box = 'user_email_login'
+        driver.find_element_by_id(email_box).send_keys("princeton.b+demo@browserstack.com")
+
+        pass_box = 'user_password'
+        driver.find_element_by_id(pass_box).send_keys(PASS)
+
+        submit_btn = 'user_submit'
+        driver.find_element_by_id(submit_btn).click()
+
+        time.sleep(5)
+
+        driver.get("https://live.browserstack.com/dashboard")
+        try:
+            WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.ID, "skip-local-installation"))
+                ).click()
+        except:
+            print("No- skip- Local")
+
+        print("Pressing Windows 10")
+        windows10 = '//*[@id="os-listing-react"]/div/div[4]/div[2]/div[1]/div/div'
+        driver.find_element_by_xpath(windows10).click()
+
+        print("Pressing Chrome")
+        xpath_chrome = '//*[@id="desktop-list-react"]/div/div[2]/div[4]/div[1]/div[6]/div/div'
+        WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, xpath_chrome))
+            ).click()
+
+        print("waiting to load")
+        time.sleep(10)
+
+        new_path = '//*[@id="flashParent"]/object'
+        element = WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located((By.XPATH, new_path))
+            )
+
+        print("executing command")
+
+        pathclose = '//*[@id="settings-menu"]/div[2]/div/div[2]/div[4]/div/div[4]/button'
+        WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.XPATH, pathclose))
+            ).click()
+
+        try:
+            pathsetting = '//*[@id="settings"]/div/label'
+            WebDriverWait(driver, 20).until(
+                    EC.presence_of_element_located((By.XPATH, pathsetting))
+                ).click()
+        except:
+            print("Didnt Find")
+            pass
+
+        id = 'flashlight-overlay'
+        element = WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located((By.ID, id))
+            )
+        element.click()
+
+        time.sleep(15)
+        print("ACTION")
+        if(desired_cap["os"] == 'OS X'):
+            action.send_keys(Keys.COMMAND, 'l').perform()
+        time.sleep(1)
+        action.reset_actions()
+
+        if(desired_cap["os"] == 'Windows'):
+            new_action = ActionChains(driver)
+            new_action.send_keys("browerstack").send_keys(Keys.ENTER).perform()
+        else:
+            new_action = ActionChains(driver)
+            new_action.send_keys("https://www.google.com/search?q=browerstack").send_keys(Keys.ENTER).perform()
+
+        time.sleep(2)
+
+        print("DONE")
+
+        time.sleep(5)
+        driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Passed!"}}')
+    except:
+        print("Errorr Occured")
+        driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Failed"}}')
+    finally:
+        driver.quit()
 
 
-# if str(driver.current_url).startswith('https://live.browserstack.com/'):
-#     pass
-# else:
-#     try:
-#         live_id = 'live_cross_product_explore'
-#         driver.find_element_by_id(live_id).click()
-#     except:
-#         live_path = '//*[@id="header"]/header/div/div/div/nav/ul/li[1]/a'
-#         driver.find_element_by_xpath(live_path).click()
-
-WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "skip-local-installation"))
-    ).click()
-
-print("Pressing Windows 10")
-windows10 = '//*[@id="os-listing-react"]/div/div[4]/div[2]/div[1]/div/div'
-driver.find_element_by_xpath(windows10).click()
-
-print("Pressing Chrome")
-xpath_chrome = '//*[@id="desktop-list-react"]/div/div[2]/div[4]/div[1]/div[4]/div/div'
-WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, xpath_chrome))
-    ).click()
-
-print("waiting to load")
-new_path = '//*[@id="flashParent"]/object'
-element = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.XPATH, new_path))
-    )
-
-print("executing command")
-
-pathclose = '//*[@id="settings-menu"]/div[2]/div/div[2]/div[4]/div/div[4]/button'
-WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.XPATH, pathclose))
-    ).click()
-
-try:
-    pathsetting = '//*[@id="settings"]/div/label'
-    WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.XPATH, pathsetting))
-        ).click()
-except:
-    print("Didnt Find")
-    pass
-
-id = 'flashlight-overlay'
-element = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.ID, id))
-    )
-
-action.key_down(Keys.CONTROL).send_keys('L').key_up(Keys.CONTROL).perform()
-
-# action.send_keys(Keys.COMMAND, 'L').perform()
-# action_chains.move_to_element_with_offset(elem, 660, 420).perform()
-# .send_keys("https://www.google.com/search?q=browerstack")\
-# .send_keys(Keys.ENTER)
-# action.perform()
-
-
-print("DONE")
-
-time.sleep(5)
-driver.quit()
-
+# run_session(caps)
+# run_session(single_caps)
+for cap in caps:
+    run_session(cap)
+# for cap in caps:
+#     Thread(target=run_session, args=(cap,)).start()
--- Percy/percy.js
+++ Percy/percy.js
@@ -1,5 +1,6 @@
 const { Builder } = require("selenium-webdriver");
 const percySnapshot = require('@percy/selenium-webdriver');
+const { Options } = require("selenium-webdriver/chrome");
 
 const baseUrl = process.env.PERCY_BRANCH == "production" ? "https://www.browserstack.com" : " https://k8s.bsstag.com"
 const endPoints = {
@@ -9,12 +10,22 @@ const endPoints = {
     "Docs":"/docs"
     }
 
+widths_per = [375, 480, 720, 1280, 1440, 1920]
+
+
+
+OPTIONS = {
+    snapshot: {
+        widths : widths_per
+    }
+}
+
 async function percy_run(){
     let driver = await new Builder().forBrowser('chrome').build();
     try{
         for(let endPoint in endPoints){
             await driver.get(baseUrl + endPoints[endPoint]);
-            await percySnapshot(driver,endPoint);
+            await percySnapshot(driver,endPoint, OPTIONS);
         }
     }
     finally{
@@ -22,5 +33,5 @@ async function percy_run(){
     }
     
 }
-
+//PERCY_BRANCH=staging PERCY_TARGET_BRANCH=production percy exec -- node percy.js
 percy_run();
